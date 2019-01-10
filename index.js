b987965ee9e34943ab0d1e5083654acbada7e825
hmudassir <muhammad.mudassir@techtronix.bix>
2019-01-10 16:45:43 +0500

    splitted the server among multiple cores

--- index.js
+++ index.js
@@ -2,53 +2,64 @@ var http = require('http');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
 var cluster = require('cluster');
+var os = require('os');
 var childProcess = require('child_process');
 
 var config = require('./config');
 
-var httpServer = http.createServer(function(req, res){
+var httpServer;
 
-    if(cluster.isMaster){
-        var parsedUrl = url.parse(req.url, true);
-        var path = parsedUrl.pathname;
-        var trimedPath = path.replace(/^\/+|\/+$/g, '');
-        var queryStringObj = parsedUrl.query;
-        var method = req.method.toLocaleLowerCase();
-        var headers = req.headers;
-        var decoder = new StringDecoder('utf-8');
-        var buffer = '';
-        req.on('data', function(data){
-            buffer += decoder.write(data);
-        });
-        req.on('end', function(){
-            buffer += decoder.end();
-
-            var chosenHandler = typeof(router[trimedPath]) != 'undefined' ? router[trimedPath] : handlers.notFound;
-
-            var data = {
-                'trimedPath': trimedPath,
-                'queryStringObj': queryStringObj,
-                'method': method,
-                'headers': headers,
-                'payload': buffer
-            };
-            chosenHandler(data, function(statusCode, payload){
-                statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-                payload = typeof(payload) == 'object' ? payload : {};
-                var payloadString = JSON.stringify(payload);
-                // To return response as json
-                res.setHeader('Content-Type', 'application/json');
-                res.writeHead(statusCode);
-                res.end(payloadString);
-            });
-        }); 
+if(cluster.isMaster){
+    for(var i=0; i < os.cpus().length; i++){
+        cluster.fork();
     }
+}else{
 
-});
+    httpServer = http.createServer(function(req, res){
 
-httpServer.listen(config.httpPort, function(){
-    console.log("server started at==>>", config.httpPort);
-});
+            var parsedUrl = url.parse(req.url, true);
+            var path = parsedUrl.pathname;
+            var trimedPath = path.replace(/^\/+|\/+$/g, '');
+            var queryStringObj = parsedUrl.query;
+            var method = req.method.toLocaleLowerCase();
+            var headers = req.headers;
+            var decoder = new StringDecoder('utf-8');
+            var buffer = '';
+            req.on('data', function(data){
+                buffer += decoder.write(data);
+            });
+            req.on('end', function(){
+                buffer += decoder.end();
+    
+                var chosenHandler = typeof(router[trimedPath]) != 'undefined' ? router[trimedPath] : handlers.notFound;
+    
+                var data = {
+                    'trimedPath': trimedPath,
+                    'queryStringObj': queryStringObj,
+                    'method': method,
+                    'headers': headers,
+                    'payload': buffer
+                };
+                chosenHandler(data, function(statusCode, payload){
+                    statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+                    payload = typeof(payload) == 'object' ? payload : {};
+                    var payloadString = JSON.stringify(payload);
+                    // To return response as json
+                    res.setHeader('Content-Type', 'application/json');
+                    res.writeHead(statusCode);
+                    res.end(payloadString);
+                });
+            }); 
+    
+    });
+    
+    httpServer.listen(config.httpPort, function(){
+        console.log("server started at==>>", config.httpPort);
+    });
+
+}
+
+
 
 //request route handler
 var handlers = {};
